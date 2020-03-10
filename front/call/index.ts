d1d98c68827444ec5e3bc3e3826848e3d5188d66
yinxulai <yinxulai@hotmail.com>
2020-03-10 23:15:03 +0800

    add file

--- front/call/index.ts
+++
@@ -1,8 +0,0 @@
-import { ipcRenderer } from 'electron'
-import { ConvertOptions } from '../../server/convert'
-
-// 单个文件转换
-export async function convert(options: ConvertOptions) {
-  return ipcRenderer.invoke('convert', options)
-}
-
--- server/handler.ts
+++
@@ -1,7 +0,0 @@
-import { ipcMain } from 'electron';
-import convert, { ConvertOptions } from './convert';
-
-// 监听通信
-ipcMain.handle('convert', (_, options: ConvertOptions) => {
-  return convert(options)
-})
