7900eacb9d1c0b6fcd93e50d8b42451d81ec42a0
yinxulai <yinxulai@hotmail.com>
2020-03-28 17:09:01 +0800

    优化 webpack 配置

--- webpack.js
+++ webpack.js
@@ -1,11 +1,13 @@
-module.exports = function(config) {
+module.exports = function (config) {
   const styleRules = config.module.rules.filter(rule =>
     rule.test.toString().match(/css|less|s\(\[ac\]\)ss/)
   )
 
   styleRules.forEach(rule => {
     const cssLoader = rule.use.find(use => use.loader === 'css-loader')
-    cssLoader.options.modules = 'local'
+    if (cssLoader) {
+      cssLoader.options = { ...cssLoader.options, modules: 'local' }
+    }
   })
 
   return config
